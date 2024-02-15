1829807265dbcca11dcc7d5ad0884cb6a4e524b4
Stephen Lavelle <analytic@gmail.com>
2024-02-15 12:42:29 +0100

    cursive font previewer

--- recitation_practice/script.js
+++ recitation_practice/script.js
@@ -56,6 +56,7 @@ function onCharHover(event,char,force=false){
 		lastselected.style.display="";
 		lastselected.parentElement.style.color="";
 		console.log("clearing lastselected " + lastselected);
+		lastselected = null;
 	}
 
 	lastselected = element;
@@ -79,10 +80,89 @@ for (var i = 0; i < chars.length; i++) {
 	},{passive: true});
 }
 
-var display_c = document.getElementsByClassName("display_c");
 
-for (var i = 0; i < display_c.length; i++) {
-	display_c[i].addEventListener("click", function(e){
+
+function onCursiveHover(event,char,force=false){
+console.log("onCursiveHover" + char+","+force);
+
+	if (char === undefined||char===null) {
+		char = event.target;
+	}
+
+	if (!char.className.includes("cursive")) {
+		return;
+	}
+
+	console.log("char on "+char+ "-"+char.className);
+
+
+	var element = char.parentElement.parentElement.parentElement.getElementsByClassName("cursivetooltip")[0];
+	if (element === undefined) {
+		console.log("no definition for " + char + "of class " + char.className);
+		return;
+	}
+
+	//char centerpoint
+	var rect = char.getBoundingClientRect();
+	var cx = rect.left + rect.width/2;
+	var cy = rect.top + rect.height/2;
+	//if cx is less than half the width of the screen, then the char is on the left side of the screen
+	var left = cx < window.innerWidth/2;
+	//if it's on the left, show the definition on the right, and vice versa
+	if (left){
+		element.style.left = "90%";
+		element.style.right = "";		
+	} else {
+		element.style.left = "";
+		element.style.right = "90%";		
+	}
+	
+
+	if (lastselected===element&&!force) {
+		console.log("already selected "+element.textContent);		
+		return;
+	}
+	
+	console.log(event.type);
+	console.log("element on "+element+ "-"+element.className);
+
+	element.style.top = "0px";
+	element.style.display="block";
+	//recalculate element bounds
+	rect = element.getBoundingClientRect();
+
+	//if bottom of element is below bottom of screen, move it up
+	if (rect.bottom > window.innerHeight) {
+		element.style.top = (window.innerHeight - rect.bottom-5) + "px";
+	}
+
+	
+	char.style.color="var(--main-punctuation-colour)";
+	if (lastselected !== null && lastselected !== element) {
+		lastselected.style.display="";
+		lastselected.parentElement.style.color="";
+		console.log("clearing lastselected " + lastselected);
+	}
+
+	lastselected = element;
+}
+var chars_cc = document.getElementsByClassName("cursive");
+
+for (var i = 0; i < chars_cc.length; i++) {
+	chars_cc[i].addEventListener("pointerover", onCursiveHover);
+	chars_cc[i].addEventListener("touchstart", onCursiveHover,{passive: true});
+	chars_cc[i].addEventListener("touchmove", function(e){
+		var touch = e.touches[0];
+		var element = document.elementFromPoint(touch.clientX, touch.clientY);
+		onCursiveHover(e,element,true);
+	},{passive: true});
+}
+
+
+var display_cr = document.getElementsByClassName("display_cr");
+
+for (var i = 0; i < display_cr.length; i++) {
+	display_cr[i].addEventListener("click", function(e){
 		console.log("touchstart");
 		var char = e.target.textContent;
 		char = char[0];
